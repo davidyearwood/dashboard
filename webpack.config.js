521485cff41f4794b472b229480ea7990d4690b2
Davidood <dwhywood@gmail.com>
2018-10-21 14:58:04 -0500

    Add image, and webpack loaders for loading styles

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/js')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
+            test: /\.css$/, 
+            use: [
+                {loader: "style-loader"}, 
+                {loader: "css-loader"}
+            ]
+        }]
+    }
+};
