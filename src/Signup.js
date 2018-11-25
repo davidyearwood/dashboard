45c5590e330572dd31b5a2846109228cd79f49e7
theboynextdoor <dwhywood@gmail.com>
2018-11-25 17:12:43 -0600

    Create JSX signup form

--- src/Signup.js
+++ src/Signup.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+
+// Form fields: 
+// Email 
+// First 
+// Last
 export default class Signup extends React.Component {
-
-
+    constructor() {
+        
+    }
     render() {
-        return <p>Hello, world</p>; 
+        return (
+            <form method="POST" action="/users"> 
+                <p className="form-field">
+                    <label for="email" className="form-field__label">Email</label>
+                    <input type="email" className="form-field__input" id="email"/> 
+                </p>
+
+                <p className="form-field">
+                    <label for="first-name" className="form-field__label">First Name</label>
+                    <input type="text" className="form-field__input" id="first-name" /> 
+                </p>
+
+                <p className="form-field"> 
+                    <label for="last-name" className="form-field__label">Last Name</label>
+                    <input type="text" className="form-field__input" id="last-name" />
+                </p>
+
+                <button type="submit">Register user</button>
+            </form> 
+        ); 
     }
 }
