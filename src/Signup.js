fdacb2ac2d7fb7058ab6080f0d6c47ba95f0dd98
theboynextdoor <dwhywood@gmail.com>
2018-11-25 22:15:27 -0600

    Design Registration Page

--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import isEmail from 'validator/lib/isEmail';
+import './stylesheet/card.css'; 
 
 // Form fields: 
 // Email 
@@ -64,31 +65,32 @@ export default class Signup extends React.Component {
         return (
         <div className="container">
             <header className="header-signup text-center">
-                <img src="./img/yodlr.svg" class="" alt="" />
+                <img src="./img/yodlr.svg" class="mb-3 logo-bg" alt="" />
             </header>
-            <div class="card mx-auto">
-                <h1 className="card-header">User Registration</h1>
+            <div class="card mx-auto card-yodlr col-md-5">
                 <div className="card-body">
+                    <h1 className="card-title h4 mt-3 mb-4 font-weight-normal">User Registration</h1>
                     <form method="POST" action="/users"> 
-                        <div className="form-group">
+                        <div className="form-group mb-3">
                             <label for="email" className="form-field__label">Email</label>
-                            <input type="email" name="email" className="form-control" id="email" value={ this.state.email.value} onChange={this.handleEmailChange}/> 
+                            <input type="email" name="email" className="form-control" id="email" placeholder="e.g., bob@gmail.com"value={ this.state.email.value} onChange={this.handleEmailChange}/> 
                         </div>
 
-                        <div className="form-group">
+                        <div className="form-group mb-3">
                             <label for="first-name" className="form-field__label">First Name</label>
-                            <input type="text" name="firstName" className="form-control" id="first-name" value={ this.state.firstName.value} onChange={this.handleFirstNameChange}/> 
+                            <input type="text" name="firstName" className="form-control" id="first-name" placeholder="e.g., Bob" value={ this.state.firstName.value} onChange={this.handleFirstNameChange}/> 
                         </div>
 
-                        <div className="form-group"> 
+                        <div className="form-group mb-3"> 
                             <label for="last-name" className="form-field__label">Last Name</label>
-                            <input type="text" name="lastName" className="form-control" id="last-name" value={ this.state.lastName.value} onChange={this.handleLastNameChange}/>
+                            <input type="text" name="lastName" className="form-control" id="last-name" placeholder="e.g., Smith"value={ this.state.lastName.value} onChange={this.handleLastNameChange}/>
                         </div>
 
-                        <button type="submit" class="btn btn-primary btn-block">Register user</button>
+                        <button type="submit" class="btn btn-primary btn-block mt-5 mb-3 btn-lg">Register user</button>
                     </form> 
                 </div>
             </div>
+            <p className="text-center mt-3"><a href="/admin.html">Admin Page</a></p>
         </div>
         ); 
     }
