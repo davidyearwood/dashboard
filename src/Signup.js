7e70425f06242507b026cb91c6b8a7d59d584cd4
theboynextdoor <dwhywood@gmail.com>
2018-11-25 18:29:02 -0600

    Users are able to register

--- src/Signup.js
+++ src/Signup.js
@@ -1,34 +1,95 @@
 import React from 'react';
-
+import isEmail from 'validator/lib/isEmail';
 
 // Form fields: 
 // Email 
 // First 
 // Last
 export default class Signup extends React.Component {
-    constructor() {
-        
+    constructor(props) {
+        super(props); 
+        this.state = {
+            firstName: {
+                value: '', 
+                hasError: false, 
+                errorMessage: null
+            }, 
+            lastName: {
+                value: '', 
+                hasError: false, 
+                errorMessage: null
+            }, 
+            email: {
+                value: '', 
+                hasError: false, 
+                errorMessage: null
+            }
+        } 
+        this.handleEmailChange = this.handleEmailChange.bind(this); 
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+        this.handleLastNameChange = this.handleLastNameChange.bind(this);
     }
+
+    handleFirstNameChange(e) {
+        this.setState({
+            firstName: {
+                value: e.target.value, 
+                hasError: false, 
+                errorMessage: null
+            }
+        });
+    }
+
+    handleLastNameChange(e) {
+        this.setState({
+            lastName: {
+                value: e.target.value, 
+                hasError: false, 
+                errorMessage: null
+            }
+        });
+    }
+
+    handleEmailChange(e) {
+        this.setState({
+            email: {
+                value: e.target.value, 
+                hasError: false, 
+                errorMessage: null
+            }
+        });
+    }
+
     render() {
         return (
-            <form method="POST" action="/users"> 
-                <p className="form-field">
-                    <label for="email" className="form-field__label">Email</label>
-                    <input type="email" className="form-field__input" id="email"/> 
-                </p>
+        <div className="container">
+            <header className="header-signup text-center">
+                <img src="./img/yodlr.svg" class="" alt="" />
+            </header>
+            <div class="card mx-auto">
+                <h1 className="card-header">User Registration</h1>
+                <div className="card-body">
+                    <form method="POST" action="/users"> 
+                        <div className="form-group">
+                            <label for="email" className="form-field__label">Email</label>
+                            <input type="email" name="email" className="form-control" id="email" value={ this.state.email.value} onChange={this.handleEmailChange}/> 
+                        </div>
 
-                <p className="form-field">
-                    <label for="first-name" className="form-field__label">First Name</label>
-                    <input type="text" className="form-field__input" id="first-name" /> 
-                </p>
+                        <div className="form-group">
+                            <label for="first-name" className="form-field__label">First Name</label>
+                            <input type="text" name="firstName" className="form-control" id="first-name" value={ this.state.firstName.value} onChange={this.handleFirstNameChange}/> 
+                        </div>
 
-                <p className="form-field"> 
-                    <label for="last-name" className="form-field__label">Last Name</label>
-                    <input type="text" className="form-field__input" id="last-name" />
-                </p>
+                        <div className="form-group"> 
+                            <label for="last-name" className="form-field__label">Last Name</label>
+                            <input type="text" name="lastName" className="form-control" id="last-name" value={ this.state.lastName.value} onChange={this.handleLastNameChange}/>
+                        </div>
 
-                <button type="submit">Register user</button>
-            </form> 
+                        <button type="submit" class="btn btn-primary btn-block">Register user</button>
+                    </form> 
+                </div>
+            </div>
+        </div>
         ); 
     }
 }
